a87c24b4122ba9f39817928da711b486d5171c9d
Adam Lonsdale <adamlonsdale@me.com>
2023-04-10 13:15:14 +0100

    Set setting based on env

--- nodered/settings.js
+++ nodered/settings.js
@@ -41,7 +41,7 @@ module.exports = {
      * node-red from being able to decrypt your existing credentials and they will be
      * lost.
      */
-    credentialSecret: "lonsdale-nodered-key",
+    credentialSecret: process.env.NODERED_CREDENTIAL_SECRET,
 
     /** By default, the flow JSON will be formatted over multiple lines making
      * it easier to compare changes when using version control.
